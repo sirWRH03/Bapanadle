e79c4627edd19c7dd814b4f5012f33558adc705a
Will Humphrey <whump1337@gmail.com>
2024-12-12 02:27:09 -0600

    Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,22 @@
 export default function Footer() {
-    return <footer>Footer!</footer>;
+    return (
+        <footer
+            style={{
+                position: "absolute",
+                left: 0,
+                bottom: 0,
+                right: 0,
+            }}
+        >
+            <hr />
+            <p style={{ display: "flex", justifyContent: "center" }}>
+                {
+                    "Based on the video game 'Hollow Knight' by Team Cherry. All assets, names, and data are the Intullectual Property of Team Cherry. Team Cherry does not offically endorse this project."
+                }
+            </p>
+            <small style={{ display: "flex", justifyContent: "center" }}>
+                Copyright © 2024 of Will Humphrey, Steven Walter, Brianna White, Alex Myrick. All rights reseverd
+            </small>
+        </footer>
+    );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+const logo = "logo.png";
+
 export default function Header() {
-    return <header>Header!</header>;
+    return (
+        <header style={{ display: "flex", justifyContent: "center", paddingBottom: 50, paddingTop: 10 }}>
+            <img style={{ height: 165, width: 606 }} src={logo} alt="Bapanadle logo" />
+        </header>
+    );
 }
