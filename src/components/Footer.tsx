b4ad0e73281905638ca99b00260673c98311ffb1
Will Humphrey <61913098+sirWRH03@users.noreply.github.com>
2024-12-12 15:30:24 -0600

    Header update (#23)
    * Update logo.png
    
    * update footer and header
    
    ---------
    
    Co-authored-by: stwalt <105939824+stwalt@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ import Divider from "@mui/material/Divider";
 export default function Footer() {
     return (
         <Box component="footer" flex="0 1 auto">
-            <p style={{ display: "flex", justifyContent: "center" }}>
+            <small style={{ display: "flex", justifyContent: "center" }}>
                 {
                     "Based on the video game 'Hollow Knight' by Team Cherry. All assets, names, and data are the Intullectual Property of Team Cherry. Team Cherry does not offically endorse this project."
                 }
-            </p>
+            </small>
             <small style={{ display: "flex", justifyContent: "center" }}>
-                Copyright © 2024 of Will Humphrey, Steven Walter, Brianna White, Alex Myrick. All rights reseverd
+                Copyright © 2024 of Will Humphrey, Steven Walter, Brianna White, Alex Myrick. All rights reserved
             </small>
         </Box>
     );
